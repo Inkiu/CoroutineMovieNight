ed0ecbee0deb9cd17997e2f6908413244a2ca7b1
inkiu <hik0115@gmail.com>
2019-07-03 17:08:56 +0900

    PopularFragment 테스트용 영화 로그

--- presentation/src/main/java/com/example/coroutinemovienight/main/popular/PopularFragment.kt
+++ presentation/src/main/java/com/example/coroutinemovienight/main/popular/PopularFragment.kt
@@ -1,24 +1,31 @@
 package com.example.coroutinemovienight.main.popular
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.coroutinemovienight.R
+import com.example.domain.usecases.GetPopularMovies
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_popular.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class PopularFragment : DaggerFragment() {
 
-    @Inject lateinit var sampleText: String
+    @Inject lateinit var getPopularMovies: GetPopularMovies
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.fragment_popular, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sampleTextView.text = sampleText
+        CoroutineScope(Dispatchers.IO).launch {
+            getPopularMovies(Unit).forEach { Log.d("tmpLog", "onViewCreated: ${it}") }
+        }
     }
 }
