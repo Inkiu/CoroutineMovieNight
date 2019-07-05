aafc1af5fc0ff49104cd07637dc738ba7c08622e
inkiu <hik0115@gmail.com>
2019-07-05 14:25:21 +0900

    MovieDetail Layout 추가

---
+++ presentation/src/main/java/com/example/coroutinemovienight/detail/MovieDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.coroutinemovienight.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coroutinemovienight.R
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
