d9350e922a7b5d95134df137562da90486015ffd
inkiu <hik0115@gmail.com>
2019-07-17 15:54:32 +0900

    lifecycle ViewModel Scope 이용으로 변경

--- presentation/src/main/java/com/example/coroutinemovienight/common/BaseViewModel.kt
+++ presentation/src/main/java/com/example/coroutinemovienight/common/BaseViewModel.kt
@@ -1,24 +1,19 @@
 package com.example.coroutinemovienight.common
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.usecases.UseCase
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel : ViewModel(), CoroutineScope {
-    private val supervisorJob = SupervisorJob()
     private val errorHandler = CoroutineExceptionHandler { _, throwable -> throwable.printStackTrace() }
 
     override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + supervisorJob + errorHandler
+        get() = viewModelScope.coroutineContext + errorHandler
 
     open fun onAttached() {}
     open fun onInitialAttached() {}
-
-    override fun onCleared() {
-        super.onCleared()
-        supervisorJob.cancel()
-    }
 
     suspend fun <P, R> UseCase<P, R>.compose(
         param: P,
